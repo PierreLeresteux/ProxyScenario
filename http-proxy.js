7f83d1e57418eba35e2c4ea1c7ecac5581858b5f
Pierre Leresteux <pierre.leresteux@gmail.com>
2014-03-10 21:52:24 +0100

    Use nedb to store path

--- http-proxy.js
+++ http-proxy.js
@@ -1,76 +1,96 @@
+var databaseName = 'database';
 var http = require('http'),
     sys = require('sys'),
     httpProxy = require('http-proxy'),
     send = require('send'),
-    mkdirp = require('mkdirp'),
     fs = require('fs'),
-    url = require('url');
+    Datastore = require('nedb'),
+    db = new Datastore({ filename: databaseName });
 
-var baseUrl='api.openweathermap.org';
-var rootPath='/Users/pierre/git/ProxyScenario/root';
+var baseUrl = 'api.openweathermap.org';
+var rootPath = '/Users/pierre/git/ProxyScenario/root';
 
-http.createServer(function (req, res) {
-    var url = req.url;
-    var method = req.method;
-    sys.puts('Call : '+method+' => '+baseUrl+url);
+db.loadDatabase(function (err) {
+    db = {};
+    db.entry = new Datastore(databaseName + '/entry.db');
+    db.entry.loadDatabase();
 
-    var options = {
-        host: baseUrl,
-        path: url,
-        method: method,
-        headers: req.headers
-    };
+    http.createServer(function (req, res) {
+        var url = req.url;
+        var method = req.method;
+        sys.puts('Call : ' + method + ' => ' + baseUrl + url);
+
+        var options = {
+            host: baseUrl,
+            path: url,
+            method: method,
+            headers: req.headers
+        };
 
 
-    callback = function(response) {
-        var str = '';
+        callback = function (response) {
+            var str = '';
 
-        //another chunk of data has been recieved, so append it to `str`
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
+            //another chunk of data has been recieved, so append it to `str`
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
 
-        response.on('end', function () {
-            console.log(str);
-            console.log(rootPath+url);
-            var split = url.split('/');
-            var dirPath=rootPath;
-            for (var i=0;i<split.length-1;i++){
-                dirPath+=split[i]+"/";
-            }
-            var fileName = split[split.length-1];
-            console.log("DirPath : "+dirPath);
-            console.log("Filename : "+fileName);
-            mkdirp.sync(dirPath);
-            fs.writeFileSync(dirPath+"/"+fileName, str);
+            response.on('end', function () {
+                console.log(str);
+                console.log(rootPath + url);
+                /*var split = url.split('/');
+                 var dirPath = rootPath;
+                 for (var i = 0; i < split.length - 1; i++) {
+                 dirPath += split[i] + "/";
+                 }
+                 var fileName = split[split.length - 1];
+                 console.log("DirPath : " + dirPath);
+                 console.log("Filename : " + fileName);
+                 mkdirp.sync(dirPath);
+                 fs.writeFileSync(dirPath + "/" + fileName, str);
+                 */
+                var entry = {
+                    method: method,
+                    url: url,
+                    body: str
+                };
+                db.entry.insert(entry);
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(str);
+                res.end();
+            });
+        }
 
+        http.request(options, callback).end();
+
+    }).listen(8000);
+
+    sys.puts('Server running at http://127.0.0.1:8000');
+
+    var app = http.createServer(function (req, res) {
+
+
+        function error(err) {
+            res.statusCode = err.status || 500;
+            res.end(err.message);
+        }
+
+        function redirect() {
+            res.statusCode = 301;
+            res.setHeader('Location', req.url + '/');
+            res.end('Redirecting to ' + req.url + '/');
+        }
+
+        var url = req.url;
+        var method = req.method;
+        db.entry.findOne({url: url, method: method}, function (err, docs) {
+            console.log(docs);
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(str);
+            res.write(docs.body);
             res.end();
         });
-    }
 
-    http.request(options, callback).end();
 
-}).listen(8000);
-
-sys.puts('Server running at http://127.0.0.1:8000');
-
-var app = http.createServer(function(req, res){
-    function error(err) {
-        res.statusCode = err.status || 500;
-        res.end(err.message);
-    }
-
-    function redirect() {
-        res.statusCode = 301;
-        res.setHeader('Location', req.url + '/');
-        res.end('Redirecting to ' + req.url + '/');
-    }
-
-    send(req, req.url)
-        .root(rootPath)
-        .on('error', error)
-        .on('directory', redirect)
-        .pipe(res);
-}).listen(8080);
+    }).listen(8080);
+});

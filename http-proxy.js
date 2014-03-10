a1623b3b7ab6286836f2fda95fe1b2a29d8ecd17
Pierre Leresteux <pierre.leresteux@gmail.com>
2014-03-10 22:34:52 +0100

    Remove unused functions

--- http-proxy.js
+++ http-proxy.js
@@ -84,20 +84,5 @@ db.loadDatabase(function (err) {
                 }
             });
         });
-
-        function error(err) {
-            res.statusCode = err.status || 500;
-            res.end(err.message);
-        }
-
-        function redirect() {
-            res.statusCode = 301;
-            res.setHeader('Location', req.url + '/');
-            res.end('Redirecting to ' + req.url + '/');
-        }
-
-
-
-
     }).listen(8080);
 });

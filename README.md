afb57e0c1450322f394b2ca45953da90812b2f35
Pierre Leresteux <pierre.leresteux@gmail.com>
2014-03-11 07:40:34 +0100

    Add offline mode and remove two server instances

--- README.md
+++ README.md
@@ -11,12 +11,19 @@ How it works
 
 Very simple, use with node to and start the server (node http-proxy.js)
 
-Two ports are availables :
-* 8000 : to record a request
-* 8080 : to call the recorded requests
+The listen port is a parameter (default : 8080).
+
+When you make for the first time a request, the app search in DB if a previous request match. If not, call the real API and store the response for the future. (It's like a static cache).
 
 The application is just a proxy to a real api (change the baseUrl value to target the api you want to store).
 
-
 You can also delete all entries, just start the application with the argument "eraseDB" :<br />
 node http-proxy.js eraseDB
+
+You can also run the application in offline mode (no call to the real api), with the argument : "offline" <br/>
+node http-proxy.js offline
+
+All arguments can be added ...<br/>
+node http-proxy.js offline eraseDB<br/>
+
+but offline and no entry in DB ... it's a not a good idea ! :-)
--- http-proxy.js
+++ http-proxy.js
@@ -5,65 +5,27 @@ var http = require('http'),
     db = new Datastore({ filename: databaseName });
 
 var baseUrl = 'api.openweathermap.org';
+var offline = false;
+var port = 8080;
 
 db.loadDatabase(function (err) {
     db = {};
     db.entry = new Datastore(databaseName + '/entry.db');
     db.entry.loadDatabase();
 
-    process.argv.forEach(function(val, index, array) {
-        if (val=='eraseDB'){
+
+    process.argv.forEach(function (val, index, array) {
+
+        if (val == 'eraseDB') {
             console.log("Erase DB");
             db.entry.remove({}, {});
-        };
-    });
-
-    http.createServer(function (req, res) {
-        var url = req.url;
-        var method = req.method;
-        var body = '';
-        req.on('data', function (data) {
-            body += data
-        });
-
-        req.on('end', function () {
-            var realReq = http.request(options, callback);
-            realReq.write(body);
-            realReq.end();
-        });
-
-        var options = {
-            host: baseUrl,
-            path: url,
-            method: method,
-            headers: req.headers
-        };
-
-
-        callback = function (response) {
-            var str = '';
-
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-
-            response.on('end', function () {
-                var entry = {
-                    method: method,
-                    url: url,
-                    bodyOut: str,
-                    bodyIn: body
-                };
-                db.entry.insert(entry);
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.write(str);
-                res.end();
-            });
         }
-
-
-    }).listen(8000);
-    console.log("Proxy listen on port 8000");
+        ;
+        if (val == 'offline') {
+            console.log("Offline mode");
+            offline = true;
+        }
+    });
     var app = http.createServer(function (req, res) {
 
         var bodyIn = '';
@@ -74,21 +36,55 @@ db.loadDatabase(function (err) {
         req.on('end', function () {
             var url = req.url;
             var method = req.method;
-
-            var query = {url: url, method: method, bodyIn:bodyIn};
+            var query = {url: url, method: method, bodyIn: bodyIn};
 
             db.entry.findOne(query, function (err, doc) {
                 if (null != doc) {
+                    console.log("Find entry : "+method+":"+url);
                     res.writeHead(200, { 'Content-Type': 'application/json' });
                     res.write(doc.bodyOut);
                     res.end();
                 } else {
-                    res.writeHead(404, { 'Content-Type': 'application/json' });
-                    res.write('{"error":"Content not yet recorded","request":'+JSON.stringify(query)+'}');
-                    res.end();
+                    if (!offline) {
+                        var options = {
+                            host: baseUrl,
+                            path: url,
+                            method: method,
+                            headers: req.headers
+                        };
+                        callback = function (response) {
+                            var str = '';
+
+                            response.on('data', function (chunk) {
+                                str += chunk;
+                            });
+
+                            response.on('end', function () {
+                                var entry = {
+                                    method: method,
+                                    url: url,
+                                    bodyOut: str,
+                                    bodyIn: bodyIn
+                                };
+                                db.entry.insert(entry);
+                                console.log("New entry recorded : "+method+":"+url);
+                                res.writeHead(200, { 'Content-Type': 'application/json' });
+                                res.write(str);
+                                res.end();
+                            });
+                        };
+                        var realReq = http.request(options, callback);
+                        realReq.write(bodyIn);
+                        realReq.end();
+
+                    } else {
+                        res.writeHead(404, { 'Content-Type': 'application/json' });
+                        res.write('{"error":"Content not yet recorded","request":' + JSON.stringify(query) + '}');
+                        res.end();
+                    }
                 }
             });
         });
-    }).listen(8080);
-    console.log("Mock API on port 8080");
+    }).listen(port);
+    console.log("Mock API on port " + port);
 });
